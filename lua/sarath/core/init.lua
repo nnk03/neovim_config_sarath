77049a72012e240670f580b2ab94f1e13b37857f
Sarath <112101057@smail.iitpkd.ac.in>
2024-09-06 20:10:21 +0000

    Added files

---
+++ lua/sarath/core/init.lua
@@ -0,0 +1,3 @@
+require("sarath.core.keymaps")
+require("sarath.core.options")
+vim.highlight.on_yank({ higroup = "IncSearch", timeout = 150 })
---
+++ lua/sarath/lazy.lua
@@ -0,0 +1,30 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+  command = "if mode() != 'c' | checktime | endif",
+  pattern = { "*" },
+})
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({ { import = "sarath.plugins" }, { import = "sarath.plugins.lsp" } }, {
+  install = {
+    colorscheme = { "nightfly" },
+  },
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false,
+  },
+})
